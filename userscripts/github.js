b205e2de0607202f95b1be580934290ed942d04a
Luzian Scherrer <luzian@neoos.ch>
2022-09-04 12:27:29 +0200

    Button injection

--- userscripts/github.js
+++ userscripts/github.js
@@ -5,14 +5,14 @@
 // @license      MIT
 // @author       Luzian Scherrer
 // @namespace    https://github.com/luzianscherrer
-// @match        https://github.com/*commits/*
+// @match        https://github.com/*
 // @icon         https://github.githubassets.com/pinned-octocat.svg
 // @grant        none
 // @require      https://unpkg.com/@metamask/detect-provider/dist/detect-provider.min.js
 // @supportURL   https://github.com/luzianscherrer/gitanchor/issues
 // ==/UserScript==
 
-
+/*
 window.buttonClicked = async function (hashValue) {
     console.log('action for: ' + hashValue);
 
@@ -42,28 +42,82 @@ function inject() {
 
     }
 }
+*/
 
 (function () {
     'use strict';
 
     console.log('Hello GitAnchor');
-    inject();
 
-    /*
-    if(location.href.match(/.\/commits/)) {
-        inject();
+    const anchorObject = `
+        <div data-view-component="true" class="BtnGroup">
+            <details class="details-reset details-overlay mr-0 mb-0 " id="branch-select-menu">
+                <summary class="btn css-truncate" data-hotkey="w" title="Switch branches or tags">
+                    <svg text="gray" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16"
+                        data-view-component="true" class="octicon octicon-git-branch">
+                        <path fill-rule="evenodd"
+                            d="M11.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122V6A2.5 2.5 0 0110 8.5H6a1 1 0 00-1 1v1.128a2.251 2.251 0 11-1.5 0V5.372a2.25 2.25 0 111.5 0v1.836A2.492 2.492 0 016 7h4a1 1 0 001-1v-.628A2.25 2.25 0 019.5 3.25zM4.25 12a.75.75 0 100 1.5.75.75 0 000-1.5zM3.5 3.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0z">
+                        </path>
+                    </svg>
+                    <span class="css-truncate-target" data-menu-button="">GitAnchor</span>
+                    <span class="dropdown-caret"></span>
+                </summary>
+
+                <div class="SelectMenu">
+                    <div class="SelectMenu-modal">
+                        <header class="SelectMenu-header">
+                            <span class="SelectMenu-title">Anchor not set</span>
+                        </header>
+
+                        <footer class="SelectMenu-footer"><a href="#">Set anchor now</a>
+                        </footer>
+
+                    </div>
+                </div>
+
+            </details>
+        </div>
+    `;
+
+
+    function htmlToElement(html) {
+        var template = document.createElement('template');
+        html = html.trim();
+        template.innerHTML = html;
+        return template.content.firstChild;
     }
 
-    const observer = new MutationObserver((mutationsList) => {
-        if(location.href.match(/.\/commits/)) {
-            mutationsList.forEach(function(mutation) {
-                console.log(mutation.type);
-            });
-            //console.log(mutationsList);
-            inject();
+
+    function inject() {
+        console.log('inject');
+        if (location.href.match(/.\/commits/)) {
+
+            var found = document.querySelectorAll("li.Box-row clipboard-copy");
+            for (var selector of found) {
+                // selector.value is the hash hash
+                var container = selector.closest('div.BtnGroup');
+
+                if (!container.classList.contains("gitanchor")) {
+                    container.after(htmlToElement(anchorObject));
+                }
+
+                container.classList.add("gitanchor");
+            }
+
         }
+    }
+
+
+    let timer;
+    const debounceInterval = 200;
+    const observer = new MutationObserver((mutationsList) => {
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            inject();
+        }, debounceInterval);
+
     });
-    observer.observe(document, {childList: true, subtree: true});
-*/
+    observer.observe(document, { childList: true, subtree: true });
 
 })();
