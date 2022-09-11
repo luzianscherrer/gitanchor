0484a775ce87e503b3253d3d14911c2758dc727b
Luzian Scherrer <luzian@neoos.ch>
2022-09-11 22:44:02 +0200

    Implemented verification

--- cli/src/commands/verify.ts
+++ cli/src/commands/verify.ts
@@ -1,3 +1,29 @@
-export function verify(hash: string) {
-    console.log(`Veryfing ${hash}`);
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from "../constants"
+import dotenv from 'dotenv';
+dotenv.config({ path: '~/.gitanchor' });
+
+export async function verify(hash: string) {
+
+    if(process.env.RPC_PROVIDER) {
+        const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
+        const gitAnchorContract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider);
+        const value = await gitAnchorContract.getAnchor(hash);
+        if(value[0].toNumber() !== 0) {
+            let displayDate = new Date(value[0].toNumber() * 1000).toLocaleString('EN', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+            });
+            console.log(`The hash ${hash} has been anchored on ${displayDate}`);
+        } else {
+            console.log(`The hash ${hash} is not anchored`);
+        }    
+    } else {
+        console.log(`No RPC_PROVIDER found in ~/.gitanchor config file`);
+    }
+
 }
---
+++ cli/src/constants.ts
@@ -0,0 +1,5 @@
+export const CONTRACT_ADDRESS = '0xC3524D9C7bb54929fd7049075Bc2fa05Ba96dF95';
+export const CONTRACT_ABI = [
+    "function getAnchor(string memory anchorHash) public view returns (uint256, address)",
+    "function setAnchor(string memory anchorHash) public"
+];
