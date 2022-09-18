ebe555051ff84a8a42c0d9506e937508619e2010
Luzian Scherrer <luzian@neoos.ch>
2022-09-18 13:48:16 +0200

    Connection handling

--- webapp/src/app/services/web3.service.ts
+++ webapp/src/app/services/web3.service.ts
@@ -7,11 +7,13 @@ import Web3Modal from "web3modal";
   providedIn: 'root'
 })
 export class Web3Service {
-  public accountsObservable = new Subject<string[]>();
+  public accountObservable = new Subject<string>();
   web3Modal: any;
+  connection: any;
   provider: any;
   signer: any;
   account: any;
+  network: any;
 
   constructor() { 
     const providerOptions = { };
@@ -27,13 +29,46 @@ export class Web3Service {
 
   }
 
-  async connectAccount() {
-    const instance = await this.web3Modal.connect();
-    this.provider = new ethers.providers.Web3Provider(instance);
+  async updateConnectionState() {
+    this.provider = new ethers.providers.Web3Provider(this.connection);
     this.signer = this.provider.getSigner();
-
     [this.account] = await this.provider.listAccounts();
-    console.log(`Connected to ${this.account}`);
+    this.network = await this.provider.getNetwork();
+    this.accountObservable.next(this.account);
+    if(this.account) {
+      console.log(`Connected to ${this.account} (${this.network.name})`);
+    } else {
+      console.log(`Not connected`);
+    }
+    
+  }
+
+  async connectAccount() {
+
+    if(this.connection) return;
+
+    this.connection = await this.web3Modal.connect();
+
+    this.connection.on("accountsChanged", (accounts: any) => {
+      console.log('accountsChanged', accounts);
+      this.updateConnectionState();
+    });
+
+    this.connection.on("chainChanged", (chainId: any) => {
+      console.log('chainChanged', chainId);
+      this.updateConnectionState();
+    });
+
+    this.connection.on("connect", (info: any) => {
+      console.log('connect', info);
+    });
+
+    this.connection.on("disconnect", (error: any) => {
+      console.log('disconnect', error);
+      this.updateConnectionState();
+    });
+
+    this.updateConnectionState();
   }
 
 }
--- webapp/src/app/verify/verify.component.ts
+++ webapp/src/app/verify/verify.component.ts
@@ -7,13 +7,28 @@ import { Web3Service } from "../services/web3.service";
   styleUrls: ['./verify.component.css']
 })
 export class VerifyComponent implements OnInit {
+  walletButtonTitle: string;
 
-  constructor(private web3: Web3Service) { }
+  constructor(private web3: Web3Service) { 
+    this.walletButtonTitle = 'Connect wallet';
+
+    web3.accountObservable.subscribe({
+      next: (v) => { 
+        console.log(`Account observer: ${v}`);
+        this.walletButtonTitle = v ? `Connected to ${this.truncateEthereumAddress(v)}` : 'Connect wallet';
+      }
+    });    
+  }
 
   ngOnInit(): void {
   }
 
-  Connect() {
+  truncateEthereumAddress(address: string) {
+    if (address.length <= 12) return address;
+    return address.substr(0, 5) + '...' + address.substr(address.length - 4);
+  };
+
+  connectWallet() {
     this.web3.connectAccount();
   }
 
