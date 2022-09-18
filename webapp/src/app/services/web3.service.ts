8f2619c075876dae51882ddf2f394455750207bb
Luzian Scherrer <luzian@neoos.ch>
2022-09-18 11:47:00 +0200

    Wallet connection

---
+++ webapp/src/app/services/web3.service.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Web3Service {
+  public accountsObservable = new Subject<string[]>();
+  web3Modal: any;
+  provider: any;
+  signer: any;
+  account: any;
+
+  constructor() { 
+    const providerOptions = { };
+
+    this.web3Modal = new Web3Modal({
+      cacheProvider: true,
+      providerOptions
+    });
+
+  }
+
+  async connectAccount() {
+    const instance = await this.web3Modal.connect();
+    this.provider = new ethers.providers.Web3Provider(instance);
+    this.signer = this.provider.getSigner();
+
+    [this.account] = await this.provider.listAccounts();
+    return this.account;
+  }
+
+}
---
+++ webapp/src/app/verify/verify.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Web3Service } from "../services/web3.service";
+
+@Component({
+  selector: 'app-verify',
+  templateUrl: './verify.component.html',
+  styleUrls: ['./verify.component.css']
+})
+export class VerifyComponent implements OnInit {
+
+  constructor(private web3: Web3Service) { }
+
+  ngOnInit(): void {
+  }
+
+  Connect() {
+    this.web3.connectAccount().then(response => {
+      console.log(response);
+    })
+  }
+
+}
